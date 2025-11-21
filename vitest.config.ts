a6ecdb9a41748c750d71ca0cbeccccffe3b9f62e
Oliver Barnwell <barnwelloliver@gmail.com>
2025-11-21 19:28:07 +0000

    example wrangler cloudflare special edition vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,6 @@
 import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config';
 
 export default defineWorkersConfig({
-  resolve: {
-    extensions: ['.ts', '.js', '.mjs', '.jsx', '.tsx', '.json'],
-  },
-  assetsInclude: ['**/*.wasm', '**/*.wasm?module'],
-  server: {
-    fs: {
-      allow: ['..'],
-    },
-  },
   test: {
     poolOptions: {
       workers: {
